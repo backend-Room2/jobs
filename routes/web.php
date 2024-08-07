95983cf689cf05c0da9efb557f23cecae5ab6996
elenmohsen <ellen.mohsen85@gmail.com>
2024-08-07 22:30:44 +0300

    new_project(jobs)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('index',[PublicController::class,'index'])->name('jobs.index');
